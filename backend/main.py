aea67c562662404a2709eda35657858594d182f4
Abdul Khader <akthetiger561@gmail.com>
2026-07-02 03:23:33 +0530

    Fix cords

--- backend/main.py
+++ backend/main.py
@@ -42,6 +42,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://localhost:5175",
         "http://localhost:3000",
+        "https://health-point-tau.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
